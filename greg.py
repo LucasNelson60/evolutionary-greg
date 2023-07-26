c4fa6bb0b2272474b2ab41c8a3053f6fd7ce70a5
Lucas Nelson <lucasmnelson2020@gmail.com>
2023-07-26 14:49:13 -0400

    Final version of evoGReg. Used to sequence entire genome.

--- greg.py
+++ greg.py
@@ -1,35 +1,32 @@
-from get_GQs import get_GQs
 from check_seq import check_seq
 from statistics import median
 
-def main(seq, max_loop, max_bulge, min_temp, seq_name):
+def main(seq, seq_name, max_loop, GQs, scores):
 
-    return_string = ""
-    
-    GQs, scores = get_GQs(max_loop, max_bulge, min_temp)
+    lines = []
     _, GRegs = check_seq(seq, GQs, scores, max_loop)
 
-    for index, info in enumerate(GRegs):
-        # print(f"info for GReg {str(index)} is {str(info)}")
+    for info in GRegs:
+        
         sequence = seq[info[1]:info[2]]
         each_multi = info[0]
         start_index = str(info[1]+1)
         end_index = str(info[2])
         length = str(info[2]-info[1])
         G_number = 0
+        
         for nucleotide in sequence:
             if nucleotide == "G":
                 G_number = G_number + 1
+                
         G_content = str(round((G_number / int(length) * 100)))
         Ntot = str(int(sum(info[0])/12))
         Ntand = str(info[4])
         Tm_min = str(round(min(info[3]),1))
         Tm_median = str(round(median(info[3]), 1))
         Tm_max = str(round(max(info[3]), 1))
-        if index != 0:
-            this_string = seq_name + "," + ";".join(sequence) + "," + ";".join(str(x) for x in each_multi) + "," + start_index + "," + end_index + "," + length + "," + G_content + "," + Ntot + "," + Ntand + "," + Tm_max + "," + Tm_median + "," + Tm_min + "\n"
-        else:
-            this_string = seq_name + "," + ";".join(sequence) + "," + ";".join(str(x) for x in each_multi) + "," + start_index + "," + end_index + "," + length + "," + G_content + "," + Ntot + "," + Ntand + "," + Tm_max + "," + Tm_median + "," + Tm_min + "," + seq + "\n"
-        return_string += this_string
         
-    return return_string
+        line = [seq_name, "".join(sequence), ";".join(str(x) for x in each_multi), start_index, end_index, length, G_content, Ntot, Ntand, Tm_max, Tm_median, Tm_min]
+        lines.append(line)
+        
+    return lines
